552328925e7ac255d6f51b29e0bafdfa1fe78261
kalayn <kalyan>
2026-03-14 10:43:29 -0500

    Initial Commit

---
+++ Posts and Comments/Posts_and_CommentsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Posts_and_CommentsApp.swift
+//  Posts and Comments
+//
+//  Created by kalyan on 3/14/26.
+//
+
+import SwiftUI
+
+@main
+struct Posts_and_CommentsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
